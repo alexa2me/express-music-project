582b503d7ea135bc69d8d5892cbd99ec80d51271
Alexandra Alcantara <senhorita.alexandra@hotmail.com>
2021-07-06 14:51:15 -0300

    Endpoints sign up and login: DONE

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,10 @@
 import { config } from "dotenv";
 import App from "./app";
-// import userHandle from "./routes/userRouter";
+import userHandle from "./routes/userRouter";
 // import songHandle from "./routes/bandRouter";
 
 config();
 
-const expressApp: App = new App([
-  /*userHandle, songHandle*/
-]);
+const expressApp: App = new App([userHandle /*songHandle*/]);
 
 expressApp.init().listen();
