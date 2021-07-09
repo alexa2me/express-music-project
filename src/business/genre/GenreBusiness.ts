eeaec2badb3510c262c2e1fd9b7a95e7b28f8efc
Alexandra Alcantara <senhorita.alexandra@hotmail.com>
2021-07-09 15:04:59 -0300

    Business do genre criado

---
+++ src/business/genre/GenreBusiness.ts
@@ -0,0 +1,35 @@
+import genreDatabase, { IGenreDatabase } from "../../data/GenreDatabase";
+import { CustomError } from "../../error/CustomError";
+import { Genre, GenreInputDTO } from "../../model/Genre";
+import authenticator, { IAuthenticator } from "../../services/Authenticator";
+import idGenerator, { IIdGenerator } from "../../services/IdGenerator";
+
+export class GenreBusiness {
+  constructor(
+    private authenticator: IAuthenticator,
+    private idGenerator: IIdGenerator,
+    private genreDatabase: IGenreDatabase
+  ) {}
+
+  async createGenre(genre: GenreInputDTO, token: string | undefined) {
+    this.validateToken(token);
+
+    const id = this.idGenerator.generate();
+
+    const newGenre = new Genre(id, genre.name);
+
+    await this.genreDatabase.createGenre(newGenre.genreToDatabase());
+
+    return "Genre created successfully!";
+  }
+
+  private validateToken(token: string | undefined) {
+    if (!token) {
+      throw new CustomError("Unauthorized", 401);
+    }
+
+    this.authenticator.getTokenData(token);
+  }
+}
+
+export default new GenreBusiness(authenticator, idGenerator, genreDatabase);
