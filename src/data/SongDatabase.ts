c44e1b382de8f2bc2eb00aee720d46d6b7840a29
Alexandra Alcantara <senhorita.alexandra@hotmail.com>
2021-07-09 15:08:34 -0300

    SongDatabase atualizado

--- src/data/SongDatabase.ts
+++ src/data/SongDatabase.ts
@@ -7,10 +7,24 @@ export interface ISongDatabase {
 
 export class SongDatabase extends BaseDatabase implements ISongDatabase {
   private static TABLE_NAME = "songs_fullstack";
+  private static TABLE_RELATIONSHIP = "songs_genres_fullstack";
 
-  public async createSong(data: SongDataDTO): Promise<void> {
+  public async createSong(song: SongDataDTO): Promise<void> {
     try {
-      await this.getConnection().insert(data).into(SongDatabase.TABLE_NAME);
+      await this.getConnection()
+        .insert(song.data)
+        .into(SongDatabase.TABLE_NAME);
+
+      await Promise.all(
+        song.genre.map(async (item) => {
+          return await this.getConnection()
+            .insert({
+              song_id: song.data.id,
+              genre_id: item,
+            })
+            .into(SongDatabase.TABLE_RELATIONSHIP);
+        })
+      );
     } catch (error) {
       throw new Error(error.sqlMessage || error.message);
     }
