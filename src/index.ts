788a71c8fc43404c3a5f56e06c4e3f74d02b08e7
Alexandra Alcantara <senhorita.alexandra@hotmail.com>
2021-07-09 15:04:26 -0300

    Adicionado genreHandle no index

--- src/index.ts
+++ src/index.ts
@@ -2,9 +2,10 @@ import { config } from "dotenv";
 import App from "./app";
 import userHandle from "./routes/userRouter";
 import songHandle from "./routes/songRouter";
+import genreHandle from "./routes/genreRouter";
 
 config();
 
-const expressApp: App = new App([userHandle, songHandle]);
+const expressApp: App = new App([userHandle, songHandle, genreHandle]);
 
 expressApp.init().listen();
